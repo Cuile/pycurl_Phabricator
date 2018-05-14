0a20b511d7114a9d22ddae345b4fea2ea18bfe2a
MI-CUILE\cuile <i@cuile.com>
2018-05-14 23:10:22 +0800

    添加 search_by_project_name 方法
    以项目名称搜索任务

--- Conduit/maniphest.py
+++ Conduit/maniphest.py
@@ -4,3 +4,8 @@
 
 def search(ph, data={}):
     return ph.call_method('maniphest.search', data)
+
+
+def search_by_project_name(ph, project_name):
+    parameters = {'queryKey': 'all', 'constraints': {"projects": [project_name]}}
+    return search(ph, parameters)
