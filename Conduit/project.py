274e36f619d9b228ad3cb389ec08858dbd1759c3
MI-CUILE\cuile <i@cuile.com>
2018-05-14 21:51:03 +0800

    添加 search_by_name 方法
    以项目名称搜索项目

--- Conduit/project.py
+++ Conduit/project.py
@@ -4,3 +4,8 @@
 
 def search(ph, data={}):
     return ph.call_method('project.search', data)
+
+
+def search_by_name(ph, name):
+    parameters = {'queryKey': 'all', 'constraints': {'name': name}}
+    return search(ph, parameters)
