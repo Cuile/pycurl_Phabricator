3df37f5984cbb54e9529087c13154280350b2b9e
MI-CUILE\cuile <i@cuile.com>
2018-05-28 20:14:04 +0800

    添加 search_by_email 方法

--- Conduit/user.py
+++ Conduit/user.py
@@ -4,3 +4,12 @@
 
 def whoami(ph, data={}):
     return ph.call_method('user.whoami', data)
+
+
+def search(ph, data={}):
+    return ph.call_method('user.search', data)
+
+
+def search_by_email(ph, email):
+    parameters = {'queryKey': 'all', 'constraints': {'usernames': [email.split('@')[0]]}}
+    return search(ph, parameters)
